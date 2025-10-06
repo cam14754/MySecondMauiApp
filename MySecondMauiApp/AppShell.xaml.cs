d3836374e462ff0832b772e8150f66c95874e3b4
Cameron Strachan <cstrachan@esri.com>
2025-10-06 11:15:10 +1100

    Initial Commit

---
+++ MySecondMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MySecondMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MySecondMauiApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MySecondMauiApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
