b4c22c0a8eeac7d019112b6eaa826311f9037a45
Cameron Strachan <cstrachan@esri.com>
2025-10-10 16:36:48 +1100

    Demo Converter and Custom Control working

--- MySecondMauiApp/Converters/ConvertToBackwards.cs
+++ MySecondMauiApp/Converters/ConvertToBackwards.cs
@@ -4,11 +4,12 @@
 
 
 namespace MySecondMauiApp.Converters;
-public partial class Class1 : IValueConverter
+public partial class ConvertToBackwards : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        string? name = value.ToString();
+        return name != null ? new string(name.Reverse().ToArray()) : null;
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
--- MySecondMauiApp/MauiProgram.cs
+++ MySecondMauiApp/MauiProgram.cs
@@ -29,7 +29,6 @@ public static class MauiProgram
         builder.Services.AddSingleton<IFileSaver>(FileSaver.Default);
         builder.Services.AddSingleton<IMediaPicker>(MediaPicker.Default);
 
-
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
