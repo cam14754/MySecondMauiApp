7f2a780c4182ee775560b9ed6dadea018405af81
Cameron Strachan <cstrachan@esri.com>
2025-10-10 15:24:19 +1100

    Used shorthand Bindable Properties

--- MySecondMauiApp/Components/MainPageButtonComponent.xaml.cs
+++ MySecondMauiApp/Components/MainPageButtonComponent.xaml.cs
@@ -1,31 +1,42 @@
-﻿namespace MySecondMauiApp.Components;
+﻿using SQuan.Helpers.Maui.Mvvm;
+
+namespace MySecondMauiApp.Components;
 
 public partial class MainPageButtonComponent : ContentView
 {
-    public static readonly BindableProperty CustomProperty1Property =
-        BindableProperty.Create(
-            propertyName: nameof(CustomProperty1),
-            returnType: typeof(string),
-            declaringType: typeof(MainPageButtonComponent));
+    //public static readonly BindableProperty CustomProperty1Property =
+    //    BindableProperty.Create(
+    //        propertyName: nameof(CustomProperty1),
+    //        returnType: typeof(string),
+    //        declaringType: typeof(MainPageButtonComponent));
 
-    public string CustomProperty1
-    {
-        get => GetValue(CustomProperty1Property) as string ?? "boo";
-        set => SetValue(CustomProperty1Property, value);
-    }
+    //public string CustomProperty1
+    //{
+    //    get => GetValue(CustomProperty1Property) as string ?? "boo";
+    //    set => SetValue(CustomProperty1Property, value);
+    //}
 
-    public static readonly BindableProperty CustomProperty2Property =
-        BindableProperty.Create(
-            propertyName: nameof(CustomProperty2),
-            returnType: typeof(int),
-            declaringType: typeof(MainPageButtonComponent),
-            defaultValue: 69);
+    //public static readonly BindableProperty CustomProperty2Property =
+    //    BindableProperty.Create(
+    //        propertyName: nameof(CustomProperty2),
+    //        returnType: typeof(int),
+    //        declaringType: typeof(MainPageButtonComponent),
+    //        defaultValue: 69);
 
-    public int CustomProperty2
-    {
-        get => (int)GetValue(CustomProperty2Property);
-        set => SetValue(CustomProperty2Property, value);
-    }
+    //public int CustomProperty2
+    //{
+    //    get => (int)GetValue(CustomProperty2Property);
+    //    set => SetValue(CustomProperty2Property, value);
+    //}
+
+
+
+    [BindableProperty]
+    public partial string CustomProperty1 { get; set; } = "boo";
+
+    [BindableProperty]
+    public partial int CustomProperty2 { get; set; } = 69;
+
 
     public MainPageButtonComponent()
     {
